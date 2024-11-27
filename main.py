a0b40b49ce1e7c1efe6a4552759c3196398192ad
Lara Nosti <nosti.yo.lara@gmail.com>
2024-11-27 01:20:22 -0300

    De la opción 2 a la 5 hay try's en caso de error

--- main.py
+++ main.py
@@ -32,19 +32,19 @@ def main():
         
         elif opcion == "2":
             os.system('cls')
-            try:
+            try: #Prueba para prevenir errores
                 nombre_coleccion = input("Ingrese el nombre de la colección: ")
                 collection = db.obtener_coleccion(nombre_coleccion)
                 ruta_csv = input("Ingrese la ruta del archivo CSV: ")
                 collection.importar_csv(ruta_csv)
                 input("\n-Presione Enter para volver al menú-")
-            except Exception as e:
+            except Exception as e: #Principalmente planeado para errores con el nombre de la colección
                 print(f"Error al igresar el nombre de la colección: {e}")
                 input("\n-Presione Enter para volver al menú-")
         
         elif opcion == "3":
             os.system('cls')
-            try:
+            try: #Prueba para prevenir errores
                 nombre_coleccion = input("Ingrese el nombre de la colección: ")
                 collection = db.obtener_coleccion(nombre_coleccion)
                 doc_id = int(input("Ingrese el ID del documento: "))
@@ -58,33 +58,41 @@ def main():
                 else:
                     print(f"Colección '{nombre_coleccion}' no encontrada.")
                 input("\n-Presione Enter para volver al menú-")
-            except Exception as e:
+            except Exception as e: #Principalmente planeado para errores con el nombre de la colección
                 print(f"Error al igresar el nombre de la colección: {e}")
                 input("\n-Presione Enter para volver al menú-")
         
-        elif opcion == "4": #Testesando
-            os.system('cls')
-            nombre_coleccion = input("Ingrese el nombre de la colección: ")
-            collection = db.obtener_coleccion(nombre_coleccion)
-            doc_id = input("Ingrese el ID del documento a eliminar: ")
-            if collection:
-                collection.eliminar_doc(doc_id)
-            input("\n-Presione Enter para volver al menú-")
+        elif opcion == "4":
+            try: #Prueba para prevenir errores
+                os.system('cls')
+                nombre_coleccion = input("Ingrese el nombre de la colección: ")
+                collection = db.obtener_coleccion(nombre_coleccion)
+                doc_id = int(input("Ingrese el ID del documento a eliminar: "))
+                if collection:
+                    collection.eliminar_doc(doc_id)
+                input("\n-Presione Enter para volver al menú-")
+            except Exception as e: #Principalmente planeado para errores con el nombre de la colección
+                print(f"Error al igresar el nombre de la colección: {e}")
+                input("\n-Presione Enter para volver al menú-")
         
-        elif opcion == "5": #Testesando
-            os.system('cls')
-            nombre_coleccion = input("Ingrese el nombre de la colección: ")
-            collection = db.obtener_coleccion(nombre_coleccion)
-            if collection:
-                documentos = collection.list_documents()
-                if documentos:
-                    print("\n--- Lista de Documentos ---")
-                    for doc in documentos:
-                        print(doc)
-                        print("-----------")
-                else:
-                    print("No hay documentos en la colección.")
-            input("\n-Presione Enter para volver al menú-")
+        elif opcion == "5":
+            try: #Prueba para prevenir errores
+                os.system('cls')
+                nombre_coleccion = input("Ingrese el nombre de la colección: ")
+                collection = db.obtener_coleccion(nombre_coleccion)
+                if collection:
+                    documentos = collection.listar_doc()
+                    if documentos:
+                        print("\n--- Lista de Documentos ---")
+                        for doc in documentos:
+                            print(doc)
+                            print("-----------")
+                    else:
+                        print("No hay documentos en la colección.")
+                input("\n-Presione Enter para volver al menú-")
+            except Exception as e: #Principalmente planeado para errores con el nombre de la colección
+                print(f"Error al igresar el nombre de la colección: {e}")
+                input("\n-Presione Enter para volver al menú-")
         
         elif opcion == "6":
             os.system('cls')
