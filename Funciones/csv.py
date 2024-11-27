4652ca503f3380bfb30a228b3406eac45b40bfe4
Lara Nosti <nosti.yo.lara@gmail.com>
2024-11-27 00:38:51 -0300

    Funcionan las primeras 3 opciones

---
+++ Funciones/csv.py
@@ -0,0 +1,56 @@
+# Limpiar pantalla
+import os
+os.system('cls')
+
+schema = "Nombre,Apellido,Edad,DNI"
+row = "Lara,Nosti,21,45.236.681"
+
+class SchemaError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+
+class Archivo(object):
+    def __init__(self, schemaStr, separator=","):
+        if len(schema) == 0:
+            raise SchemaError("El schema está vacío")
+        self.schema = schemaStr.split(separator)
+        self.separator = separator
+    
+    def convert(self, row):
+        tmp = row.split(self.separator)
+        if len(tmp) == len(self.schema):
+            i = 0
+            d = {}
+            while i < len(tmp):
+                key = self.schema[i]
+                val = tmp[i]
+                d[key] = val
+                i += 1
+            return d
+        else:
+            raise SchemaError("Los campos de la fila no concuerdan con el schema")
+    
+    # Otra cosa
+    def PrintTmp(self):
+        print(self.tmp)
+
+try:
+    o = Archivo(2)
+    d = o.convert(row)
+    print(d)
+except AttributeError:
+    print("Parámetros incorrectos")
+except SchemaError as e:
+    print("Fallo algo en el schema:", e)
+
+
+o = Archivo(schema)
+print(o.schema)
+
+d = o.convert(row)
+print(d["DNI"])
+
+# Otra cosa
+o.tmp = "Jose"
+o.PrintTmp()
--- Funciones/opciones.py
+++ Funciones/opciones.py
@@ -3,46 +3,60 @@ Opciones
 
 Conección con las clases
 '''
-#from nomPY import nomClase
+from .csv import Archivo
 
 # Limpiar pantalla
 import os
 os.system('cls')
 
 class Documento:
-    #Genera el id icremental con cada documento que se le agregue
+    #Genera el id incremental
     autoinc = 0
     def __init__(self, contenido=None):
         self.id = Documento.autoinc
         Documento.autoinc += 1
         self.contenido = contenido if contenido is not None else {}
     
-    def obtener_valor(self, clave):
-        return self.contenido.get(clave, None)
-    
-    def modificar_valor(self, clave, valor):
-        self.contenido[clave] = valor
-    
     def __str__(self):
-        return f"Documento {self.id}, contenido: {self.contenido}"
+        return f"Contenido: {self.contenido}"
 
 class Coleccion:
     def __init__(self, nombre):
         self.nombre = nombre
         self.documentos = {}
     
-    def aniadir_d(self, docu):
+    #2. Importar CSV a colección
+    def importar_csv(self, ruta):
+        if not os.path.exists(ruta):
+            print(f"El archivo {ruta} no existe.")
+            return
+        try:
+            with open(ruta, "rt") as file:
+                schema = file.readline().replace("\n", "")
+                csv = Archivo(schema)
+                for line in file:
+                    nuevo_doc = Documento(csv.convert(line.strip("\n"))) #El strip borra el \n que sale a lo último
+                    self.aniadir_doc(nuevo_doc)
+                print(f"CSV de la ruta '{ruta}' importado correctamente.")
+        except Exception as e:
+            print(f"Error al importar el archivo: {e}")
+    
+    def aniadir_doc(self, docu):
         self.documentos[docu.id] = docu
     
-    def eliminar_d(self, id_docu):
+    #3. Consultar documento en colección
+    def buscar_doc(self, id_docu):
+        return self.documentos.get(id_docu, None)
+    
+    #4. Eliminar documento de colección
+    def eliminar_doc(self, id_docu):
         if id_docu in self.documentos:
             del self.documentos[id_docu]
     
-    def buscar_d(self, id_docu):
-        return self.documentos.get(id_docu, None)
-    
+    #5. Listar todos los documentos en colección
     def __str__(self):
-        return f"Colección {self.nombre}, con: {len(self.documentos)} documentos"
+        return Documento.str()
+        #return f"Colección {self.nombre}, con: {len(self.documentos)} documentos"
 
 class BaseDeDatos:
     def __init__(self):
@@ -50,48 +64,12 @@ class BaseDeDatos:
     
     #1. Crear colección
     def crear_coleccion(self, nombre_coleccion):
-        if nombre_coleccion not in self.colecciones:
+        if nombre_coleccion in self.colecciones:
+            print(f"La colección '{nombre_coleccion}' ya existe.")
+        else:
             self.colecciones[nombre_coleccion] = Coleccion(nombre_coleccion)
+            print(f"Colección '{nombre_coleccion}' creada con éxito.")
     
-    #2. Importar CSV a colección
-    
-    #3. Consultar documento en colección
-    
-    #4. Eliminar documento de colección
-    
-    #5. Listar todos los documentos en colección
+    #Acción principal de la que deriva el funcionamiento las otras 4
     def obtener_coleccion(self, nombre_coleccion):
         return self.colecciones[nombre_coleccion]
-    
-    '''
-    def eliminar_coleccion(self, nombre_coleccion):
-        if nombre_coleccion in self.colecciones:
-            del self.colecciones[nombre_coleccion]
-    
-    def __str__(self):
-        return f"Base de datos documental con {len(self.colecciones)} colecciones"
-    '''
-
-
-
-
-
-
-
-
-db = BaseDeDatos()
-c = Coleccion('muniecas')
-m1 = Documento({'marca': 'Barbie', 'estetica': 'personalizable'})
-m2 = Documento({'marca': 'Monster High', 'estetica': 'gótica'})
-c.aniadir_d(m1)
-c.aniadir_d(m2)
-print(str(c))
-
-munieca = c.buscar_d(1)
-print(munieca.obtener_valor('marca'), munieca.obtener_valor('estetica'))
-c.eliminar_d(munieca.id)
-munieca = c.buscar_d(1)
-if munieca is not None:
-    print(munieca.obtener_valor('marca'), munieca.obtener_valor('estetica'))
-else:
-    print("La muñeca no existe más")
